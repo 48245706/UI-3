7b989517b53480e47bc07425254467aa4c2ad0da
48245706 <48245706@est.ort.edu.ar>
2024-05-29 10:41:16 -0300

    Escena foquito

---
+++ Assets/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text txtFruta;
+    public string nombreFruta;
+    // lograr que al presionar el botón se muestre un nombre al azar
+    // de un array de al menos 4 elementos
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateFruta()
+    {
+        txtFruta.text = nombreFruta;
+    } 
+}
